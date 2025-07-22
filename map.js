07d4e39564b8d367f109cf163fd389609c5d7a72
Karol Salvador <110723595+KarolSalvador@users.noreply.github.com>
2025-07-22 19:11:44 -0300

    Add files via upload

---
+++ map.js
@@ -0,0 +1,4 @@
+//// 1. Use map para criar um array de strings formatadas como
+//    "Nome do produto - Categoria" para cada produto
+// 2. Use map para criar um array com os preços de todos os produtos com impostos (15% a mais)
+// 3. Combine forEach com um acumulador externo para calcular o valor total de todos os produtos
